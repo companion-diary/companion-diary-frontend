2de7b05c0bb1df14869b9dac9d36c18f8b30a8e1
Yuhee Kim <dbgml4509@naver.com>
2022-12-01 15:15:17 +0900

    feat :  AlertDialog 스타일 테마 추가

--- app/src/main/java/com/example/companion_diary/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/companion_diary/login/view/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.companion_diary.login.view
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,9 +9,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.companion_diary.MainActivity
-import com.example.companion_diary.login.model.KakaoAuthViewModel
-import com.example.companion_diary.login.api.TokenNetworkService
 import com.example.companion_diary.databinding.ActivityLoginBinding
+import com.example.companion_diary.login.api.TokenNetworkService
+import com.example.companion_diary.login.model.KakaoAuthViewModel
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.kakao.sdk.user.UserApiClient
 import kotlinx.coroutines.Dispatchers
@@ -69,7 +70,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun loginAlertDialog(context : Context) {
         MaterialAlertDialogBuilder(context)
-            .setTitle("카카오 로그인 오류")
+            .setTitle("로그인 오류")
             .setMessage("카카오 로그인 오류가 발생하였습니다.\n다시 로그인해주세요.")
             .setPositiveButton("확인") { dialog, which ->
             }
